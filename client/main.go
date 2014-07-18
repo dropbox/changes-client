79316bca4581f01a49d2993afe280174b2d7c644
Rajat Goel <rajat.goel2010@gmail.com>
2014-07-18 15:43:59 -0700

    Add version

--- client/main.go
+++ client/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"flag"
+    "fmt"
 
 	"github.com/dropbox/changes-client"
 )
 
+const (
+    Version = "0.0.4"
+)
+
 func main() {
+    showVersion := flag.Bool("version", false, "Prints changes-client version")
 	flag.Parse()
+
+    if *showVersion {
+        fmt.Println(Version)
+        return
+    }
 
 	config, err := runner.GetConfig()
 	if err != nil {
