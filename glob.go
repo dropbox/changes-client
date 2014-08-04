2c94ad56d1460c15b6c5ee2c6e917d19e0159ce9
David Cramer <cramer@dropbox.com>
2014-08-04 16:54:31 -0700

    Add recursive glob for artifacts

---
+++ glob.go
@@ -0,0 +1,33 @@
+// Copyright 2010 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package runner
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func GlobTree(root string, patterns []string) ([]string, error) {
+	matches := []string{}
+
+	visit := func(path string, f os.FileInfo, err error) error {
+		pathBits := strings.Split(path, "/")
+		filename := pathBits[len(pathBits)-1]
+		for _, pattern := range patterns {
+			m, err := filepath.Match(pattern, filename)
+			if err != nil {
+				return err
+			}
+			if m {
+				matches = append(matches, path)
+			}
+		}
+		return nil
+	}
+
+	err := filepath.Walk(root, visit)
+	return matches, err
+}
