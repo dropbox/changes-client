22c6114855036fab87bdda31a19dbcf169a2de37
David Cramer <cramer@dropbox.com>
2014-10-29 12:38:11 -0700

    Add support for Sentry

--- client.go
+++ client.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"log"
 
 	"github.com/dropbox/changes-client/client"
 	"github.com/dropbox/changes-client/engine"
+	"github.com/getsentry/raven-go"
 )
 
 const (
 	Version = "0.0.8"
+)
+
+var (
+	sentryDsn = ""
 )
 
 func main() {
@@ -21,10 +28,45 @@ func main() {
 		return
 	}
 
+	if sentryDsn != "" {
+		sentryClient, err := raven.NewClient(sentryDsn, map[string]string{
+			"version": Version,
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer func() {
+			var packet *raven.Packet
+			switch rval := recover().(type) {
+			case nil:
+				return
+			case error:
+				packet = raven.NewPacket(rval.Error(), raven.NewException(rval, raven.NewStacktrace(2, 3, nil)))
+			default:
+				rvalStr := fmt.Sprint(rval)
+				packet = raven.NewPacket(rvalStr, raven.NewException(errors.New(rvalStr), raven.NewStacktrace(2, 3, nil)))
+			}
+
+			_, ch := sentryClient.Capture(packet, map[string]string{})
+			<-ch
+		}()
+
+		run()
+	} else {
+		run()
+	}
+}
+
+func run() {
 	config, err := client.GetConfig()
 	if err != nil {
 		panic(err)
 	}
 
 	engine.RunBuildPlan(config)
+}
+
+func init() {
+	flag.StringVar(&sentryDsn, "sentry-dsn", "", "Sentry DSN for reporting errors")
 }
