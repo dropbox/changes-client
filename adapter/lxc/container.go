e8003e22f3e28a9991c6226c03e21c4691b8c3e0
Jared Hance <jhance@dropbox.com>
2015-06-23 15:42:55 -0700

    Document container functions
    Summary:
    Add some documents to the container functions, in particular documenting
    the procedure we use to create a new container from scratch/from snapshot
    since nobody previously seemed to know what actually happens in the full
    stack.
    
    Test Plan: Doc.
    
    Reviewers: jukka
    
    Reviewed By: jukka
    
    Subscribers: mkedia, vishal
    
    Differential Revision: https://tails.corp.dropbox.com/D117673

--- adapter/lxc/container.go
+++ adapter/lxc/container.go
@@ -84,6 +84,46 @@ func (c *Container) acquireLock(name string) (*lockfile.Lockfile, error) {
 	}
 }
 
+// In this phase we actually launch the container that the tests
+// will be run in.
+//
+// There are essentially four cases:
+//  - we aren't using a snapshot
+//  - we are using a snapshot but don't have it cached
+//  - we are using a cached snapshot but don't have a base container
+//  - we are using a cached snapshot and have a base container
+//
+// The first case is clearly different from the latter three, and indeed
+// the process it follows is rather different because it doesn't use the
+// same template. In the first case, without a snapshot, we use the
+// ubuntu template (see /usr/share/lxc/templates) to create a container,
+// and destroy it at the end. Only the first run of this will be extremely
+// slow to create the container itself, but after that it will be faster,
+// although it still must pay a heavy cost for provisioning.
+//
+// For the latter three cases, it follows a general process saving work
+// where work doesn't have to be done. First, it checks if we have a
+// snapshot base container or not. If we do, it clones it using overlayfs
+// and then starts the resulting container; the existing base container
+// is not modified.
+//
+// If we don't have a base container, then it checks for a compressed
+// tarball of the filesystem. This is either are .tar.xz or .tar.lz4
+// and the compression must match what compression changes-client is
+// being used for. If this file doesn't exist, the client fetches it
+// from the given s3 bucket in a folder qualified by its arch, dist,
+// release, and snapshot id.
+//
+// Once we have guaranteed that we have a snapshot image, the snapshot
+// image is loaded using the "download" template (or a variant for it
+// if we are using the faster lz4 compression). This template will
+// require the image already to be cached - as it can't download it
+// like normal - so we use --force-cached as a template option. Once
+// the base container is up, we proceed as normal, and we leave the
+// base container alive so that future runs are fast.
+//
+// Once the container is started, we mount the container and perform
+// basic configurations as well as run the pre-launch script.
 func (c *Container) launchContainer(clientLog *client.Log) error {
 	var err error
 	var base *lxc.Container
@@ -246,6 +286,13 @@ func (c *Container) launchContainer(clientLog *client.Log) error {
 	return nil
 }
 
+// Takes care of the entire launch process as opposed to up to the
+// start of the launch process (above). After the container
+// starts up networking at the end of launchContainer,
+// we install certificates, update the apt-get cache,
+// set up sudoers, and finally run the post-launch script
+// which will provision the system (and depends on the completion
+// of the above three tasks).
 func (c *Container) Launch(clientLog *client.Log) error {
 	var err error
 
@@ -297,6 +344,9 @@ func (c *Container) Stop() error {
 	return nil
 }
 
+// Destroys the container. We ensure that this is called as long
+// as we don't have --keep-container as an option or we don't get
+// SIGKILLed.
 func (c *Container) Destroy() error {
 	// Destroy must operate idempotently
 	var err error
@@ -322,6 +372,9 @@ func (c *Container) Destroy() error {
 	return nil
 }
 
+// Our CI scripts run as the user ubuntu but may need access
+// to root. In order to get around this, we just give all users
+// in the container access to root as this is no less dangerous.
 func (c *Container) setupSudoers() error {
 	sudoersPath := path.Join(c.RootFs(), "etc", "sudoers")
 	f, err := os.Create(sudoersPath)
@@ -353,6 +406,9 @@ func randString(n int) string {
 	return string(bytes)
 }
 
+// Runs a command in a container. This "uploads" the command to the container,
+// essentially copying the command from the host to the container filesystem,
+// and then runs the new temporary file, capturing output.
 func (c *Container) RunCommandInContainer(cmd *client.Command, clientLog *client.Log, user string) (*client.CommandResult, error) {
 	dstFile := fmt.Sprintf("/tmp/script-%s", randString(10))
 
@@ -378,10 +434,13 @@ func (c *Container) RunCommandInContainer(cmd *client.Command, clientLog *client
 	return cw.Run(cmd.CaptureOutput, clientLog, c.lxc)
 }
 
+// Gets the image path associated with a specific snapshot.
 func (c *Container) getImagePath(snapshot string) string {
 	return fmt.Sprintf("ubuntu/%s/amd64/%s", c.Release, snapshot)
 }
 
+// Checks to see if an existing snapshot is cached. This does not
+// refer to the tarball but rather to the base container.
 func (c *Container) snapshotIsCached(snapshot string) bool {
 	for _, name := range lxc.ContainerNames() {
 		if snapshot == name {
@@ -450,6 +509,12 @@ func (c *Container) ensureImageCached(snapshot string, clientLog *client.Log) er
 	return nil
 }
 
+// Creates the rootfs tarball and all other metadata that the lxc-download
+// template expects. This allows us to "act" like an image that the lxc-download
+// template would download, but in fact is something entirely different that just
+// needs to be treated similarly. The download template expects images to be stored
+// on some sort of official server (not s3), but uses cached images when available.
+// The image we are creating is to be used as a cached image for the download template.
 func (c *Container) CreateImage(snapshot string, clientLog *client.Log) error {
 	var err error
 
@@ -503,6 +568,8 @@ func (c *Container) createImageMetadata(snapshotPath string, clientLog *client.L
 	return nil
 }
 
+// Compresses the root of the filesystem into the desired compressed tarball.
+// The compression here can vary based on flags.
 func (c *Container) createImageRootFs(snapshotPath string, clientLog *client.Log) error {
 	rootFsTxz := path.Join(snapshotPath, fmt.Sprintf("rootfs.tar.%s", c.Compression))
 
@@ -543,6 +610,10 @@ func (c *Container) createImageSnapshotID(snapshotPath string, clientLog *client
 	return nil
 }
 
+// Uploads a snapshot outcome to an s3 bucket, at the same path that
+// changes-client will expect to download it from. The snapshot itself
+// is just a tarball of the rootfs of the container - compressed with
+// either xz for high compression or lz4 for raw speed.
 func (c *Container) UploadImage(snapshot string, clientLog *client.Log) error {
 	relPath := c.getImagePath(snapshot)
 	localPath := fmt.Sprintf("/var/cache/lxc/download/%s", relPath)
@@ -567,6 +638,10 @@ func (c *Container) UploadImage(snapshot string, clientLog *client.Log) error {
 	return nil
 }
 
+// Runs the prelaunch script which is essentially responsible for setting up
+// the environment for the post-launch script. It runs within the host
+// environment with the container mounted at LXC_ROOTFS. Runs as the user
+// that changes-client runs as (usually root).
 func (c *Container) runPreLaunch(clientLog *client.Log) error {
 	preEnv := []string{fmt.Sprintf("LXC_ROOTFS=%s", c.RootFs()), fmt.Sprintf("LXC_NAME=%s", c.Name)}
 	cw := client.NewCmdWrapper([]string{c.PreLaunch}, "", preEnv)
@@ -582,6 +657,12 @@ func (c *Container) runPreLaunch(clientLog *client.Log) error {
 	return nil
 }
 
+// Runs the post launch script which is responsible for the provisioning
+// of the base container. This is generally costly only for non-snapshotted
+// builds as the base container is relatively static.
+//
+// This runs within the container environment, not the host. Runs as the user
+// 'ubuntu'.
 func (c *Container) runPostLaunch(clientLog *client.Log) error {
 	cw := &LxcCommand{
 		Args: []string{c.PostLaunch},
