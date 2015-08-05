d9541444e06cc5aeb126e7136ffc381a9dfd6592
Kyle Consalus <kylec@dropbox.com>
2015-08-05 16:34:31 -0700

    Ensure all pending Sentry reports finish before exiting
    Summary:
    Our individual sentry helper methods don't wait for the report to be sent, and if we
    wait for pending reports before terminating the process (which is what this diff does)
    they don't have to.
    Also a slight refactoring so that the `defer`s can reliably fire, which they can't when we
    call `os.Exit` rather than returning.
    
    Test Plan: Not much. Suggestions?
    
    Reviewers: anupc
    
    Reviewed By: anupc
    
    Subscribers: mkedia, anupc
    
    Differential Revision: https://tails.corp.dropbox.com/D129150

--- client.go
+++ client.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/dropbox/changes-client/client"
 	"github.com/dropbox/changes-client/common/sentry"
@@ -14,12 +13,9 @@ import (
 	"github.com/getsentry/raven-go"
 )
 
-var (
-	exitResult = false
-)
-
 func main() {
 	showVersion := flag.Bool("version", false, "Prints changes-client version")
+	exitResult := flag.Bool("exit-result", false, "Determine exit code from result")
 	flag.Parse()
 
 	if *showVersion {
@@ -27,33 +23,39 @@ func main() {
 		return
 	}
 
-	if sentryClient := sentry.GetClient(); sentryClient != nil {
-		defer func() {
-			var packet *raven.Packet
-			p := recover()
-			switch rval := p.(type) {
-			case nil:
-				return
-			case error:
-				packet = raven.NewPacket(rval.Error(), raven.NewException(rval, raven.NewStacktrace(2, 3, nil)))
-			default:
-				rvalStr := fmt.Sprint(rval)
-				packet = raven.NewPacket(rvalStr, raven.NewException(errors.New(rvalStr), raven.NewStacktrace(2, 3, nil)))
-			}
-
-			log.Printf("[client] Sending panic to Sentry")
-			_, ch := sentryClient.Capture(packet, map[string]string{})
-			<-ch
-			panic(p)
-		}()
-
-		run()
-	} else {
-		run()
+	success := run()
+	if !success && *exitResult {
+		log.Fatal("[client] exit: 1")
 	}
+	log.Println("[client] exit: 0")
 }
 
-func run() {
+// Returns whether run was successful.
+func run() bool {
+	if sentryClient := sentry.GetClient(); sentryClient != nil {
+		// Don't return until we're finished sending to Sentry.
+		defer sentryClient.Wait()
+		// Ensure main thread panics are caught and reported.
+		defer func() {
+			if p := recover(); p != nil {
+				var err error
+				switch rval := p.(type) {
+				case error:
+					err = rval
+				default:
+					err = errors.New(fmt.Sprint(rval))
+				}
+				packet := raven.NewPacket(err.Error(), raven.NewException(err, raven.NewStacktrace(2, 3, nil)))
+				log.Printf("[client] Sending panic to Sentry")
+				_, ch := sentryClient.Capture(packet, map[string]string{})
+				<-ch
+				panic(p)
+			}
+		}()
+	}
+
+	// Error handling in place; now we begin.
+
 	config, err := client.GetConfig()
 	if err != nil {
 		panic(err)
@@ -62,15 +64,7 @@ func run() {
 	result, err := engine.RunBuildPlan(config)
 	log.Printf("[client] Finished: %s", result)
 	if err != nil {
-		log.Printf("[client] error: %s", err.Error())
+		log.Printf("[client] error: %s", err)
 	}
-	if (err != nil || result != engine.RESULT_PASSED) && exitResult {
-		log.Printf("[client] exit: 1")
-		os.Exit(1)
-	}
-	log.Printf("[client] exit: 0")
-}
-
-func init() {
-	flag.BoolVar(&exitResult, "exit-result", false, "Determine exit code from result")
+	return err == nil && result == engine.RESULT_PASSED
 }
