c980d5ea66483adcf4979aab3867e81b182fc2bd
Kyle Consalus <kylec@dropbox.com>
2015-08-26 09:26:33 -0700

    Generate JUnit output from test results
    Summary:
    Gives the benefit of actually reporting tests, and possibly importantly, counting the step
    as failing (thus avoiding deb generation) if tests fail.
    
    Test Plan: Tried it
    
    Reviewers: anupc
    
    Reviewed By: anupc
    
    Subscribers: changesbot, mkedia, anupc
    
    Differential Revision: https://tails.corp.dropbox.com/D133491

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
-export PATH=/usr/local/go/bin:$PATH
 export GOPATH=~/
+export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
 WORKSPACE=$GOPATH/src/github.com/dropbox/changes-client
 cd $WORKSPACE
-sudo PATH=$PATH GOPATH=$GOPATH make test
+go get -v github.com/jstemmer/go-junit-report
+sudo PATH=$PATH GOPATH=$GOPATH `which go` test ./... -timeout=120s -v -race | tee test.log
+echo Generating junit.xml...
+go-junit-report -set-exit-code < test.log > junit.xml
