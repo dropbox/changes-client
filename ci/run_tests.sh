d9e41fb4e38a8be6023601b1318e6306d839f7d6
Kyle Consalus <kylec@dropbox.com>
2015-08-31 12:29:44 -0700

    Ensure ci/run_tests fails if tests don't run properly
    Summary:
    Prior to this, a failing test was required to fail the build, and type errors
    in tests would be ignored.
    
    Test Plan: Tried it on Changes
    
    Reviewers: anupc
    
    Reviewed By: anupc
    
    Subscribers: changesbot, mkedia
    
    Differential Revision: https://tails.corp.dropbox.com/D134637

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash -eu
 export GOPATH=~/
 export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
 WORKSPACE=$GOPATH/src/github.com/dropbox/changes-client
@@ -10,5 +10,8 @@ echo Done.
 (vet -shadow -shadowstrict  . 2>&1 | grep -v "declaration of err") || true
 go get github.com/jstemmer/go-junit-report
 sudo CHANGES=1 PATH=$PATH GOPATH=$GOPATH `which go` test ./... -timeout=120s -v -race | tee test.log
+EXIT_CODE=${PIPESTATUS[0]}
 echo Generating junit.xml...
-go-junit-report -set-exit-code < test.log > junit.xml
+go-junit-report < test.log > junit.xml
+echo Done.
+exit ${EXIT_CODE}
