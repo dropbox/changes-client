ba9fa6d3627e495aeb9879fa2505b4a5fdca676f
Nathaniel Herman <nate@dropbox.com>
2015-10-09 10:49:57 -0700

    Make Jenkins push snapshot status the same way as Mesos
    Summary: This is purely for uniformity between the two but shouldn't change any functionality.
    
    Test Plan: local VM.
    
    Reviewers: kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot, jukka, mkedia, anupc
    
    Differential Revision: https://tails.corp.dropbox.com/D145095

--- reporter/jenkins/reporter.go
+++ reporter/jenkins/reporter.go
@@ -1,9 +1,7 @@
 package jenkinsreporter
 
 import (
-	"encoding/json"
 	"flag"
-	"io/ioutil"
 	"log"
 	"os/exec"
 	"path"
@@ -22,15 +20,6 @@ type Reporter struct {
 	artifactDestination string
 }
 
-// This structure is used purely for json marshalling. The fields have to
-// be public otherwise the json marshaller will ignore them, but this
-// results in their automatically generated fields using capital letters
-// so we set them back to lowercase manually.
-type SnapshotResponse struct {
-	Image  string `json:"image"`
-	Status string `json:"status"`
-}
-
 func (r *Reporter) Init(c *client.Config) {
 	log.Printf("[reporter] Construct reporter with artifact destination: %s", artifactDestination)
 	r.artifactDestination = artifactDestination
@@ -41,29 +30,6 @@ func (r *Reporter) PushJobstepStatus(status string, result string) {
 }
 
 func (r *Reporter) PushCommandStatus(cID string, status string, retCode int) {
-}
-
-// In order to actually update the snapshot image status, we simply produce
-// a json file and rely on Changes to recognize this as a "critical artifact"
-// and change the status for us. Note that this artifact is produced directly
-// in the artifact destination and as such is not affected by publish artifacts.
-func (r *Reporter) PushSnapshotImageStatus(imageID string, status string) {
-	log.Printf("[reporter] image-id = %s, status = %s", imageID, status)
-	response, err := json.Marshal(SnapshotResponse{Image: imageID, Status: status})
-	if err != nil {
-		/* if this happens its an error in changes-client and not use-case
-		 * so in theory we don't have to worry about err ever being non-nil,
-		 * but report it anyway in case theres a bug
-		 */
-		log.Printf("[reporter] Failed to encode snapshot reponse.")
-		return
-	}
-	err = ioutil.WriteFile(path.Join(r.artifactDestination, "snapshot_status.json"), response, 0644)
-	if err != nil {
-		log.Printf("[reporter] Failed to write snapshot_status.json")
-		log.Printf("    was writing to directory %s", r.artifactDestination)
-		log.Printf("    error: %s", err.Error())
-	}
 }
 
 func (r *Reporter) PushLogChunk(source string, payload []byte) bool {
