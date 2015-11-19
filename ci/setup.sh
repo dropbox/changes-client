e1e510217bd6a7387dc8ca1675836ad4ad187c65
Kyle Consalus <kylec@dropbox.com>
2015-11-19 09:33:01 -0800

    Don't try to avoid Changes container puppet
    Summary:
    This way, we get the updated aptly endpoint, and we
    set a better example.
    
    Test Plan: Changes
    
    Reviewers: anupc
    
    Reviewed By: anupc
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D154917

--- ci/setup.sh
+++ ci/setup.sh
@@ -10,9 +10,3 @@ sudo cp -r . $WORKSPACE
 cd $WORKSPACE
 sudo PATH=$PATH GOPATH=$GOPATH make dev
 sudo PATH=$PATH GOPATH=$GOPATH go get golang.org/x/tools/cmd/vet
-
-# On Changes, don't bother running container puppet once setup has
-# already run.
-if [ ! -z $CHANGES ]; then
-    touch /home/ubuntu/SKIP_CITOOLS_PUPPET
-fi
