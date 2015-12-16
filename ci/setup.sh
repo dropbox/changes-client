509cf9957bfda59f223ffd123424430b11506911
Anup Chenthamarakshan <anupc@dropbox.com>
2015-12-16 11:10:29 -0800

    Don't sudo if not absolutely necessary. Don't copy stuff into a separate workspace.
    Test Plan: Hoping to land this and verify on Changes
    
    Reviewers: kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D160884

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -e
 
-sudo -E support/bootstrap-ubuntu.sh
-sudo chown -R `whoami` ~
+support/bootstrap-ubuntu.sh
 export PATH=/usr/local/go/bin:$PATH
 export GOPATH=~/
-WORKSPACE=$GOPATH/src/github.com/dropbox/changes-client
-mkdir -p `dirname $WORKSPACE`
-sudo cp -r . $WORKSPACE
-cd $WORKSPACE
-sudo PATH=$PATH GOPATH=$GOPATH make dev
-sudo PATH=$PATH GOPATH=$GOPATH go get golang.org/x/tools/cmd/vet
+cd $GOPATH/src/github.com/dropbox/changes-client
+PATH=$PATH GOPATH=$GOPATH make dev
+PATH=$PATH GOPATH=$GOPATH go get golang.org/x/tools/cmd/vet
