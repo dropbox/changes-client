043039da1493d3a8c99e97f2d890e078227ff688
Kyle Consalus <kylec@dropbox.com>
2015-12-18 12:36:41 -0800

    Only download/install Go if necessary
    Summary: Saves a few second, more consistent with how we run other Go builds.
    
    Test Plan: Changes
    
    Reviewers: nate, anupc
    
    Reviewed By: anupc
    
    Subscribers: changesbot, anupc
    
    Differential Revision: https://tails.corp.dropbox.com/D161581

--- support/bootstrap-ubuntu.sh
+++ support/bootstrap-ubuntu.sh
@@ -22,11 +22,18 @@ sudo apt-get install -y git mercurial pkg-config wget
 sudo apt-get install -y awscli
 
 # Install go
-cd /tmp
-wget "http://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
-sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
-echo 'export PATH=/usr/local/go/bin:$PATH' | sudo tee /etc/profile.d/golang.sh
-echo 'export GOPATH=~/' | sudo tee /etc/profile.d/gopath.sh
+if [ ! -x /usr/local/go/bin/go ]
+then
+  echo "Installing Go binary...."
+  cd /tmp
+  wget "http://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+  sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
+  echo "Installed Go binary...."
+else
+  echo "Go binary already installed"
+fi
+
+/usr/local/go/bin/go version
 
 # Install lxc
 sudo apt-get install -y libcgmanager0 lxc lxc-dev
