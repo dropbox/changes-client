a19c2b40d59709c0ef855e48f652521223c0d5a9
Anup Chenthamarakshan <anupc@dropbox.com>
2016-04-14 09:57:31 -0700

    Fix snapshot creation.
    Summary: `go build ./...` fails without `vet` package already fetched.
    
    Reviewers: paulruan, kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D187955

--- ci/setup.sh
+++ ci/setup.sh
@@ -5,4 +5,3 @@ export PATH=/usr/local/go/bin:$PATH
 export GOPATH=~/
 cd $GOPATH/src/github.com/dropbox/changes-client
 PATH=$PATH GOPATH=$GOPATH make dev
-PATH=$PATH GOPATH=$GOPATH go get golang.org/x/tools/cmd/vet
