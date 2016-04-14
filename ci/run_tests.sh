8ae1b766e78392068afa1bc8bb59cdc4d63a0d6e
Anup Chenthamarakshan <anupc@dropbox.com>
2016-04-14 10:22:41 -0700

    Fix calls to `vet` :(
    Test Plan: Changes
    
    Reviewers: kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot
    
    Differential Revision: https://tails.corp.dropbox.com/D187965

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -3,11 +3,15 @@ export GOPATH=~/
 export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
 WORKSPACE=$GOPATH/src/github.com/dropbox/changes-client
 cd $WORKSPACE
+
 echo Running vet...
-vet -all .
+# TODO(anupc): Remove -printf=false
+# Fails with 'adapter/lxc/cmd_wrapper.go:44: arg fn in printf call is a function value, not a function call'
+go tool vet -all -printf=false .
 echo Done.
 # report non-'err' shadows
-(vet -shadow -shadowstrict  . 2>&1 | grep -v "declaration of err") || true
+(go tool vet -shadow -shadowstrict  . 2>&1 | grep -v "declaration of err") || true
+
 go get github.com/jstemmer/go-junit-report
 sudo CHANGES=1 PATH=$PATH GOPATH=$GOPATH `which go` test -bench . ./... -timeout=120s -v -race | tee test.log
 EXIT_CODE=${PIPESTATUS[0]}
